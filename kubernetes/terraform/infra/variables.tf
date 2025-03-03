28c71123ebabebae8d44248202b3a5d1a339dcda
Mauro Uberti <maurouberti@hotmail.com>
2025-03-03 18:16:43 -0300

    fix

---
+++ kubernetes/terraform/infra/variables.tf
@@ -0,0 +1,23 @@
+variable "do_token" {
+  type = string
+}
+
+variable "do_version" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "droplet_size" {
+  type = string
+}
+
+variable "min_nodes" {
+  type = number
+}
+
+variable "max_nodes" {
+  type = number
+}
